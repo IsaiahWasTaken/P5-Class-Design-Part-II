d945296ff7ef36f205bdba936ab0a3b29650c143
IsaiahWasTaken <98197804+IsaiahWasTaken@users.noreply.github.com>
2022-02-23 01:40:02 -0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include "Section A.h"
+#include "Section B.h"
+#include "Section C.h"
+#include <iostream>
+#include <string>
+
+
+int main(){
+
+    const string input = ("https://www.ornage.com/lobster");
+    const A obj(input);
+    cout <<extfunction<<endl;
+
+
+    B call("potato");
+    call.setDate(1,1,1000);
+    call.setflash(true);
+    call.setiso(1000);
+    call.setauthorName("bob");
+    call.setxposure(111);
+    call.setType("GIF");
+    call.setXY(105,100);
+    call.setaptSize(22);
+    call.setimgSize(92385.3);
+    cout << extfunction << endl;
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
+};
